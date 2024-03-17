ab065240577bee5b7d285192d7138f95ac222265
shapiro <shapiro@hellomarket.com>
2024-03-17 17:40:27 +0900

    💄 [UI]: SearchViewController SearchBar 구현

--- ItBookDaangn/Scene/Search/ViewController/SearchViewController.swift
+++ ItBookDaangn/Scene/Search/ViewController/SearchViewController.swift
@@ -17,11 +17,19 @@ import UIKit
 
 final class SearchViewController: UIViewController {
     //MARK: - Property
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "책을 검색해주세요"
+        searchBar.backgroundImage = UIImage()
+        searchBar.delegate = self
+        return searchBar
+    }()
     
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupUI()
     }
     
     //MARK: - Initializer
@@ -32,5 +40,46 @@ final class SearchViewController: UIViewController {
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("This class does not support NSCoder")
+    }
+}
+
+//MARK: - SetupUI
+extension SearchViewController {
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        buildHierarachy()
+        setupConstraint()
+    }
+    
+    private func buildHierarachy() {
+        view.addSubview(searchBar)
+    }
+    
+    private func setupConstraint() {
+        let safeAreaLayoutGuide = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8),
+            searchBar.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            searchBar.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            searchBar.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(false)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = nil
+        searchBar.resignFirstResponder()
     }
 }
